6db73ac484df9e1e3a180142c25aa3fce1c45bd8
Scott Parry <hello@ikreativ.com>
2012-08-17 00:03:10 +0100

    modified notices to remove left right borders as theyre full width

--- system/cms/themes/pyrocms/views/admin/partials/notices.php
+++ system/cms/themes/pyrocms/views/admin/partials/notices.php
@@ -1,41 +1,41 @@
 <?php if ($this->session->flashdata('error')): ?>
-<div class="block-message block-message-error alert">
+<div style="border-left:none;border-right:none;" class="block-message block-message-error alert">
 	<?php echo $this->session->flashdata('error'); ?>
 </div>
 <?php endif; ?>
 
 <?php if (validation_errors()): ?>
-<div class="block-message block-message-error alert">
+<div style="border-left:none;border-right:none;" class="block-message block-message-error alert">
 	<?php echo validation_errors(); ?>
 </div>
 <?php endif; ?>
 
 <?php if ( ! empty($messages['error'])): ?>
-<div class="block-message block-message-error alert">
+<div style="border-left:none;border-right:none;" class="block-message block-message-error alert">
 	<?php echo $messages['error']; ?>
 </div>
 <?php endif; ?>
 
 <?php if ($this->session->flashdata('notice')): ?>
-<div class="block-message block-message-warning alert">
+<div style="border-left:none;border-right:none;" class="block-message block-message-warning alert">
 	<?php echo $this->session->flashdata('notice');?>
 </div>
 <?php endif; ?>
 
 <?php if ( ! empty($messages['notice'])): ?>
-<div class="block-message block-message-warning alert">
+<div style="border-left:none;border-right:none;" class="block-message block-message-warning alert">
 	<?php echo $messages['notice']; ?>
 </div>
 <?php endif; ?>
 
 <?php if ($this->session->flashdata('success')): ?>
-<div class="block-message block-message-success alert">
+<div style="border-left:none;border-right:none;" class="block-message block-message-success alert">
 	<?php echo $this->session->flashdata('success'); ?>
 </div>
 <?php endif; ?>
 
 <?php if ( ! empty($messages['success'])): ?>
-<div class="block-message block-message-success alert">
+<div style="border-left:none;border-right:none;" class="block-message block-message-success alert">
 	<?php echo $messages['success']; ?>
 </div>
 <?php endif; ?>
